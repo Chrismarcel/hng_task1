c8af42c8d101be0833d2ea6387920a621cc550f1
Chrismarcel James <cmarcelj@gmail.com>
2018-04-10 09:49:10 +0100

    My submission for the HNG Task 2 by Christopher James

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HNG Internship Task 1</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel='stylesheet' type='text/css' />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div class="container top-bar">
+        <div class="nav">
+            <a class="hng-logo" href="https://hotels.ng/"><img src="assets/img/hng_logo.png" alt="Hotels.NG Homepage"></a>
+            <span class="current-time">
+                <?=date('F d, Y h:iA', time())?>
+            </span>
+        </div>
+    </div>
+    <div class="container list-body">
+        <div class="task-list">
+            <h4 class="task-header">HNG 4 INTERNSHIP, TASK 1</h4>
+            <ul>
+                <li>
+                    1. Design a very simple page in Figma
+                </li>
+                <li>
+                    2. Implement it in html/css (this design should have space showing the current time)
+                </li>
+                <li>
+                    3. Setup docker
+                </li>
+                <li>
+                    4. Get php to run in docker
+                </li>
+                <li>
+                    5. Make your app work on your local browser
+                </li>
+                <li>
+                    6. Your app must show properly
+                </li>
+                <li>
+                    7. You will need to submit a screenshot to pass to Stage 2 
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
